fc2d402b94436e3f39976264f3d15a2ed23a9ff7
ShilpaLalwani <shilpal.2507@gmail.com>
2020-04-04 13:21:52 +0530

    7 files modified, 1 deleted

--- src/app/home/articleList.ts
+++ src/app/home/articleList.ts
@@ -13,7 +13,7 @@ export const ARTICLE_MOCK: Article[] = new Array(3)
       ({
         id: i,
         title: `Lorem ipsum.`,
-        url:`https://github.com/ShilpaLalwani/routing-animation/blob/master/src/img1.jpg`,
+        url:`https://images.unsplash.com/photo-1422393462206-207b0fbd8d6b?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80`,
         subtitle: `More lorem ipsum bacon ipsum.`,
         description: `Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken.`,
         body:`Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken.
